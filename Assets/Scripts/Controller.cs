64609180f81e4fac5a1675879c7531d14abdc4b2
Fuzzonaut <nukacoladrinker.76@gmail.com>
2026-01-30 17:48:47 +0300

    Update on Controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,6 +5,11 @@ public class Controller : MonoBehaviour
     private Rigidbody2D rb;
     private Collider2D col;
     [SerializeField] private float speed = 5.0f;
+    private float mx;
+    private float my;
+    private Vector2 mousePos;
+
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,6 +20,7 @@ public class Controller : MonoBehaviour
     void Update()
     {
         Movement();
+
     }
 
 
@@ -23,8 +29,31 @@ public class Controller : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
             
-        rb.linearVelocity = new Vector2 (horizontalInput * speed, verticalInput * speed); 
-            
+        rb.linearVelocity = new Vector2 (horizontalInput * speed, verticalInput * speed);
+
+
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        float a = Mathf.DeltaAngle(0f, angle); // normalize to [-180, 180]
+
+        // snap to nearest 45°
+        float snapped = Mathf.Round(a / 45f) * 45f;
+
+        // optional: unify 180 and -180
+        if (snapped == 180f) snapped = -180f;
+
+        transform.localRotation = Quaternion.Euler(0f, 0f, snapped);
+
+
+
+
+
+
+
+
+
     }
 
 
